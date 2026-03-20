82462d2b3b73bae1a1dd08bc5a85123fbd73b052
aayushgvp-cmyk <aayush.gvp@gmail.com>
2026-03-20 20:12:35 +0530

    Fixed '' parsing as NaN

--- JS/GraphLoader.js
+++ JS/GraphLoader.js
@@ -17,31 +17,20 @@ const topLabelsPlugin = {
     }
 };
 
-
+let dataIncome
 async function handleChartAsync() {
-await new Promise(r=>setTimeout(r,5000))
+
 console.log("chart start")
-/*
-  const data = [
-    { year: "HI", count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
-*/
-//await handleFileAsync(document.getElementById('excelInput').value)
-const data=verticalsObject
+
+dataIncome=verticalsObjectIncome
 
 
 let max=0
-Object.values(data).forEach(r=>r.value>max?max=r.value:max+=0)
+Object.values(dataIncome).forEach(r=>r.value>max?max=r.value:max+=0)
 
 
-let shownChart=  new Chart(
-    document.getElementById('chartHTML'),
+let chartIncome=  new Chart(
+    document.getElementById('chartIncomeHTML'),
     {
       type: 'bar',
       options: {
@@ -58,13 +47,13 @@ let shownChart=  new Chart(
         },
 	responsive: true,
                 onClick: (e) => {
-                    const activePoints = shownChart.getElementsAtEventForMode(e, 'nearest', {intersect: true}, false);
+                    const activePoints = chartIncome.getElementsAtEventForMode(e, 'nearest', {intersect: true}, false);
                     if (activePoints.length > 0) {
                         const index = activePoints[0].index;
-                        const label = shownChart.data.labels[index];
-                        const value = shownChart.data.datasets[0].data[index];
-			setCToColour(shownChart,"#0000FF")
-			zoomIntoChart()
+                        const label = chartIncome.data.labels[index];
+                        const value = chartIncome.data.datasets[0].data[index];
+console.log(label)
+			zoomIntoChart(label,chartIncome)
                        
                     }
                 }
@@ -72,12 +61,57 @@ let shownChart=  new Chart(
       },
 	plugins:[topLabelsPlugin],
       data: {
-        labels:Object.keys(data).map(r => r),
+        labels:Object.keys(dataIncome).map(r => r),
         datasets: [
           {
             label: 'Acquisitions by year',
-            data: Object.values(data).map(row => row.value),
+            data: Object.values(dataIncome).map(row => row.value),
 		backgroundColor:'#FF0000'
+          }
+        ]
+      },
+
+    }
+  );
+
+											//Place correct data.Currently using income data
+let chartExpense=  new Chart(
+    document.getElementById('chartExpenseHTML'),
+    {
+      type: 'pie',
+      options: {
+        animation: true,
+	//scales:{y: {max: 2**Math.ceil(Math.log2(max))}},
+        plugins: {
+		title:{display:true,text:"Monthly Income"},
+          legend: {
+            display: false
+          },
+          tooltip: {
+            enabled: false
+          }
+        },
+	/*responsive: true,
+                onClick: (e) => {
+                    const activePoints =  chartExpense.getElementsAtEventForMode(e, 'nearest', {intersect: true}, false);
+                    if (activePoints.length > 0) {
+                        const index = activePoints[0].index;
+                        const label =  chartExpense.data.labels[index];
+                        const value =  chartExpense.data.datasets[0].data[index];
+			setCToColour( chartExpense,"#0000FF")
+			zoomIntoChart()
+                       
+                    }
+                }*/
+
+      },
+	plugins:[topLabelsPlugin],
+      data: {
+        labels:Object.keys(dataIncome).map(r => r),
+        datasets: [
+          {
+            label: 'Acquisitions by year',
+            data: Object.values(dataIncome).map(row => row.value),
           }
         ]
       },
--- JS/SheetWorker.js
+++ JS/SheetWorker.js
@@ -1,34 +1,21 @@
-let rawData
-
-
-
-
-
-
-let verticalsObject={};
-
+let verticalsObjectIncome={};
 async function handleFileAsync() {
-
-
-
-
-const url="https://opensheet.elk.sh/1LkcqbW144O2qQ6OZ11qLpcW1RsLH2G1Q7h2AipZXHFc/Expense"
+const url="https://opensheet.elk.sh/1itzoaXD8WNcb3U7R5anh7jHasr5S9iZcCJ0wmJNeySw/Income"
   const response = await fetch(url);    
   if (!response.ok) throw new Error('Network response was not ok');
-
-
-
  const arrayOfObjects = await response.json();
-
 const headers = Object.keys(arrayOfObjects[0]);
 
- const rawData = arrayOfObjects.map(obj =>
+ let rawData = arrayOfObjects.map(obj =>
             headers.map(key => obj[key]))
 
+rawData=rawData.filter(r=>(r[3]))
+
+rawData.forEach(r=>{if(1){r[3]=r[3].replaceAll(",","")}})
+
+
+
 rawData.forEach(r=>r[3]=parseInt(r[3]))
-
-
-rawData?log("Loaded"):log("Error")
 console.log(rawData)
 const verticalCol=1
 const seminarCol=2
@@ -38,33 +25,42 @@ const amountCol=3
 
 	rawData.forEach((row,rowNumber)=>
 	
-	{if(`${row[verticalCol]}` in verticalsObject){}
-	else if(rowNumber+1){verticalsObject[row[verticalCol]]={value:0}}
+	{if(`${row[verticalCol]}` in verticalsObjectIncome){}
+	else if(rowNumber+1){verticalsObjectIncome[row[verticalCol]]={value:0}}
 	
 	if(rowNumber+1){
-	verticalsObject[row[verticalCol]]['value']+=row[amountCol]
+	verticalsObjectIncome[row[verticalCol]]['value']+=row[amountCol]
 	}
 	})
-console.log(verticalsObject)
 //End of first nesting
 //Start of second nesting
 	rawData.forEach((row,rowNumber)=>
 	{if(rowNumber+1){
-	if(`${row[5]}` in verticalsObject[row[verticalCol]]){}
-	else {verticalsObject[row[verticalCol]][row[seminarCol]]={value:0}}
-	verticalsObject[row[verticalCol]][row[seminarCol]].value+=row[amountCol]
+	if(`${row[5]}` in verticalsObjectIncome[row[verticalCol]]){}
+	else {verticalsObjectIncome[row[verticalCol]][row[seminarCol]]={value:0}}
+	verticalsObjectIncome[row[verticalCol]][row[seminarCol]].value+=row[amountCol]
 	}})
-console.log(verticalsObject)
+console.log(verticalsObjectIncome)
 //End of second nesting	
 handleChartAsync()
-return new Promise((resolve)=>{setTimeout(()=>{console.log("Sheet function finished");resolve()},2000)})
+
 }
+
 handleFileAsync()
+
 let nestLevel=0
-function zoomIntoChart(){
-nestLevel++
-nestLevel===3?Show('chartDiv',0):log(nestLevel)
+function zoomIntoChart(vertical,chartName){
+
+nestLevel+=1
+nestLevel===3?Show('chartIncomeDiv',0):{}
+console.log(dataIncome)
+nestLevel===1?dataIncome=verticalsObjectIncome[vertical]:{}
+
+delete dataIncome.value
+console.log(dataIncome)
+chartName.update()
 }
+
 function setCToColour(chartName,colour){
 chartName.data.datasets[0].backgroundColor=colour
 chartName.update()
